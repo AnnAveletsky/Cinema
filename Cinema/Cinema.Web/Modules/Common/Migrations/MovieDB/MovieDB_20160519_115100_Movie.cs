49b7c39269ead92173f7969772ce5d406b15faf4
Аня Серба <annserba94@gmail.com>
2016-09-24 03:23:50 +0300

    add migrade

--- Cinema/Cinema.Web/Modules/Common/Migrations/MovieDB/MovieDB_20160519_115100_Movie.cs
+++ Cinema/Cinema.Web/Modules/Common/Migrations/MovieDB/MovieDB_20160519_115100_Movie.cs
@@ -1,25 +1,26 @@
 ﻿using FluentMigrator;
-using FluentMigrator.Builders.Create.Table;
 using System;
 
 namespace Cinema.Migrations.MovieDB
 {
-
-    [Migration(201609024140000)]
-    public class MovieDB_201609024_140000_Initial : AutoReversingMigration
+    [Migration(20160519115100)]
+    public class MovieDB_20160519_115100_MovieTable : Migration
     {
         public override void Up()
         {
             Create.Schema("mov");
+
             Create.Table("Movie").InSchema("mov")
-                .WithColumn("MovieId").AsInt32()
-                    .Identity().PrimaryKey().NotNullable()
+                .WithColumn("MovieId").AsInt32().Identity().PrimaryKey().NotNullable()
                 .WithColumn("Title").AsString(200).NotNullable()
                 .WithColumn("Description").AsString(1000).Nullable()
                 .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
                 .WithColumn("Year").AsInt32().Nullable()
                 .WithColumn("ReleaseDate").AsDateTime().Nullable()
                 .WithColumn("Runtime").AsInt32().Nullable();
+        }
+        public override void Down()
+        {
         }
     }
 }
---
+++ Cinema/Cinema.Web/Modules/Common/Migrations/MovieDB/MovieDB_20160519_145500_MovieKind.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Cinema.Migrations.MovieDB
+{
+    [Migration(20160519145500)]
+    public class MovieDB_20160519_145500_MovieKind : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Movie").InSchema("mov")
+                .AddColumn("Kind").AsInt32().NotNullable()
+                    .WithDefaultValue(1);
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}

f836c3f1777680e388249d6bb136fd9efeb75a31
Аня Серба <annserba94@gmail.com>
2016-10-08 17:15:26 +0300

    add files to movie

---
+++ Movie/App_Start/SiteInitialization.cs
@@ -0,0 +1,37 @@
+﻿namespace Cinema.Movie
+{
+    using Administration;
+    using Serenity;
+    using Serenity.Abstractions;
+    using Serenity.Data;
+    using System;
+    using System.Configuration;
+
+    public static partial class SiteInitialization
+    {
+        public static void ApplicationStart()
+        {
+            try
+            {
+                SqlSettings.AutoQuotedIdentifiers = true;
+                Serenity.Web.CommonInitialization.Run();
+                
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+                throw;
+            }
+
+            foreach (var databaseKey in databaseKeys)
+            {
+                EnsureDatabase(databaseKey);
+                RunMigrations(databaseKey);
+            }
+        }
+
+        public static void ApplicationEnd()
+        {
+        }
+    }
+}

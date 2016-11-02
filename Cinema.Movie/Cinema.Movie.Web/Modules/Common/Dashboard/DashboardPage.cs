47a6b3f0fe83bd6cba9f3d89d73a60f535362cc2
Аня Серба <annserba94@gmail.com>
2016-11-02 23:52:06 +0300

    add movie and href genre

--- Cinema.Movie/Cinema.Movie.Web/Modules/Common/Dashboard/DashboardPage.cs
+++ Cinema.Movie/Cinema.Movie.Web/Modules/Common/Dashboard/DashboardPage.cs
@@ -1,82 +1,140 @@
 ﻿
 namespace Cinema.Movie.Common.Pages
 {
-
-    using Movie;
     using Movie.Entities;
     using Serenity;
     using Serenity.Data;
-    using Serenity.Services;
     using System;
+    using System.Collections.Generic;
     using System.Web.Mvc;
-    using System.Web.Security;
 
     [RoutePrefix("Dashboard"), Route("{action=index}")]
     public class DashboardController : Controller
     {
-        DashboardPageModel cachedModel = TwoLevelCache.GetLocalStoreOnly("DashboardPageModel", TimeSpan.FromMinutes(5),
-                GenreRow.Fields.GenerationKey, () =>
-                {
-                    var model = new DashboardPageModel();
-                    using (var connection = SqlConnections.NewFor<GenreRow>())
-                    {
-                        model.Genres = connection.List<GenreRow>();
-                    }
-                    model.Widgets = new System.Collections.Generic.SortedList<string, string>();
-                    return model;
-                });
-        [HttpGet, Route("~/")]
-        public ActionResult Index(int? page)
+        private DashboardPageModel model = new DashboardPageModel() { Widgets = Widgets(), Genres = Genres() };
+
+        private static SortedList<string, string> Widgets()
         {
-            if (!cachedModel.Widgets.Keys.Contains("Dashboard/Dashboard"))
+           return TwoLevelCache.GetLocalStoreOnly("Widgets", TimeSpan.FromMinutes(200),
+              GenreRow.Fields.GenerationKey, () =>
+              {
+                  SortedList<string, string> Widgets = new SortedList<string, string>();
+                  Widgets.Add("Catalog", LocalText.Get("Site.Widget.Catalog"));
+                  Widgets.Add("Slider", LocalText.Get("Site.Widget.Slider"));
+                  return Widgets;
+              });
+        }
+        private static List<GenreRow> Genres()
+        {
+            return TwoLevelCache.GetLocalStoreOnly("Genres", TimeSpan.FromMinutes(200),
+              GenreRow.Fields.GenerationKey, () =>
+              {
+                  using (var connection = SqlConnections.NewFor<GenreRow>())
+                  {
+                      return connection.List<GenreRow>();
+                  }
+              });
+        }
+        private static List<MovieRow> MoviesShot(Pagination pagination)
+        {
+            using (var connection = SqlConnections.NewFor<MovieRow>())
             {
-                cachedModel.Widgets.Add("Dashboard/Dashboard", LocalText.Get("Site.Widget.Slider"));
+                if(connection.Count<MovieRow>()< pagination.Count)
+                {
+                    return connection.List<MovieRow>();
+                }
+                return connection.List<MovieRow>(i => i.Skip((pagination.Page - 1) * pagination.Count).Take(pagination.Count));
             }
-            cachedModel.Content = LocalText.Get("Site.Dashboard.HTMLDashboard");
+        }
+        private static MovieRow MovieFull(int id)
+        {
+            return TwoLevelCache.GetLocalStoreOnly("Movie"+id, TimeSpan.FromMinutes(200),
+              MovieRow.Fields.GenerationKey, () =>
+              {
+                  using (var connection = SqlConnections.NewFor<MovieRow>())
+                  {
+                      return connection.ById<MovieRow>(id);
+                  }
+              });
+        }
+        [HttpGet, Route("~/")]
+        public ActionResult Index(int count=10,int page=1,string genre="")
+        {
+            model.Content = TwoLevelCache.GetLocalStoreOnly("Dashboard/Dashboard", TimeSpan.FromMinutes(200),
+            GenreRow.Fields.GenerationKey, () =>
+            {
+                return LocalText.Get("Site.Dashboard.HTMLDashboard");
+            });
+            model.Movies = MoviesShot(new Pagination(count, page) { Genre = genre });
             ViewData["Title"] = "";
             ViewData["Footer"] = "";
             ViewData["PageId"] = "Dashboard/Dashboard";
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
         }
         [HttpGet, Route("~/catalog-films")]
-        public ActionResult CatalogFilms(string begin)
+        public ActionResult CatalogFilms(int count = 10, int page = 1)
         {
-            if (!cachedModel.Widgets.Keys.Contains("Dashboard/CatalogFilms"))
+            model.Content = TwoLevelCache.GetLocalStoreOnly("Dashboard/CatalogFilms", TimeSpan.FromMinutes(200),
+            GenreRow.Fields.GenerationKey, () =>
             {
-                cachedModel.Widgets.Add("Dashboard/CatalogFilms", LocalText.Get("Site.Widget.Catalog"));
-            }
-            cachedModel.Content = LocalText.Get("Site.Dashboard.HTMLCatalogFilms");
+                return LocalText.Get("Site.Dashboard.HTMLCatalogFilms");
+            });
+            model.Movies = MoviesShot(new Pagination(count, page));
             ViewData["Title"] = LocalText.Get("Navigation.Dashboard/CatalogFilms");
             ViewData["Footer"] = "";
-            ViewData["PageId"] = LocalText.Get("Navigation.Dashboard/CatalogFilms");
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            ViewData["PageId"] = "Dashboard/CatalogFilms";
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
         }
         [HttpGet, Route("~/top")]
-        public ActionResult Top()
+        public ActionResult Top(int count = 100, int page = 1)
         {
-            cachedModel.Content = LocalText.Get("Site.Dashboard.HTMLTop");
+            model.Content = TwoLevelCache.GetLocalStoreOnly("Dashboard/Top", TimeSpan.FromMinutes(200),
+            GenreRow.Fields.GenerationKey, () =>
+            {
+                return LocalText.Get("Site.Dashboard.HTMLTop");
+            });
+            model.Movies = MoviesShot(new Pagination(count, page));
             ViewData["Title"] = LocalText.Get("Navigation.Dashboard/Top");
             ViewData["Footer"] = "";
             ViewData["PageId"] = "Dashboard/Top";
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
         }
         [HttpGet, Route("~/faq")]
         public ActionResult FAQ()
         {
-            cachedModel.Content = LocalText.Get("Site.Dashboard.HTMLFAQ");
+            model.Content = TwoLevelCache.GetLocalStoreOnly("Dashboard/FAQ", TimeSpan.FromMinutes(200),
+            GenreRow.Fields.GenerationKey, () =>
+            {
+                return LocalText.Get("Site.Dashboard.HTMLFAQ");
+            });
             ViewData["Title"] = LocalText.Get("Navigation.Dashboard/FAQ");
             ViewData["Footer"] = "";
             ViewData["PageId"] = "Dashboard/FAQ";
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
         }
         [HttpGet, Route("~/contacts")]
         public ActionResult Contacts()
         {
-            cachedModel.Content = LocalText.Get("Site.Dashboard.HTMLContacts");
+            model.Content = TwoLevelCache.GetLocalStoreOnly("Dashboard/Contacts", TimeSpan.FromMinutes(200),
+            GenreRow.Fields.GenerationKey, () =>
+            {
+                return LocalText.Get("Site.Dashboard.HTMLContacts");
+            });
             ViewData["Title"] = LocalText.Get("Navigation.Dashboard/Contacts");
             ViewData["Footer"] = "";
             ViewData["PageId"] = "Dashboard/Contacts";
-            return View(MVC.Views.Common.Dashboard.DashboardIndex, cachedModel);
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
         }
+
+        [HttpGet, Route("~/movie")]
+        public ActionResult Movie(int id)
+        {
+            model.Movie = MovieFull(id);
+            ViewData["Title"] = LocalText.Get("Navigation.Movie/Movie");
+            ViewData["Footer"] = "";
+            ViewData["PageId"] = "Dashboard/Top";
+            return View(MVC.Views.Common.Dashboard.DashboardIndex, model);
+        }
+
     }
 }
--- Cinema.Movie/Cinema.Movie.Web/Modules/Common/Dashboard/DashboardPageModel.cs
+++ Cinema.Movie/Cinema.Movie.Web/Modules/Common/Dashboard/DashboardPageModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using Cinema.Movie.Movie.Entities;
+using System;
 using System.Collections.Generic;
 
 namespace Cinema.Movie.Common
@@ -9,5 +10,21 @@ namespace Cinema.Movie.Common
         public List<GenreRow> Genres { get; set; }
         public SortedList<string,string> Widgets { get; set; }
         public string Content { get; set; }
+        public MovieRow Movie{ get; set; }
+        public List<MovieRow> Movies { get; set; }
+    }
+    public class Pagination
+    {
+        public string Genre { get; set; }
+        public int Count { get; private set; }
+        public int Page { get; private set; }
+        public char? Begin { get; set; }
+        public string Search { get; set; }
+
+        public Pagination(int? count=10, int? page=1)
+        {
+            Count = count == null ? 10 : (int)count;
+            Page= page == null ? 1 : (int)page;
+        }
     }
 }
